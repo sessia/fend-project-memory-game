662d88b7dd5eb8375a5d880b884d171671dfea9d
Alessia <alessandri.alessia@gmail.com>
2018-04-20 22:16:49 +0200

    solved bug with timer and rating not resetting

--- js/app.js
+++ js/app.js
@@ -3,14 +3,14 @@ let movesCounter = 0;
 const deck = document.querySelector ('.deck');
 let cards = document.getElementsByClassName('card');
 let matchedCards = document.getElementsByClassName("match");
-const stars = document.getElementsByClassName('fa-star');
+let stars = document.getElementsByClassName('fa-star');
+let emptyStars = document.getElementsByClassName('fa-star-o');
 const restart = document.querySelector(".restart");
 
 //timer variables
 let timer = document.querySelector(".timer");
 let second = 0;
 let minute = 0;
-let hour = 0;
 let interval;
 let minutes = document.querySelector(".minutes");
 let seconds = document.querySelector(".seconds");
@@ -38,10 +38,38 @@ const iconsList = [
 //array of clicked cards
 let flippedCardList = [];
 
-//array of matched cards
-let matchesCounter = 0;
+//modals
+const winnerModal = document.getElementById('winnerModal');
+const loserModal = document.getElementById('loserModal');
 
-const modal = document.getElementById('winnerModal');
+//Function to start the game
+function startGame(){
+  /*shuffle cards*/
+  shuffle(iconsList);
+  /*empty the deck if it already exists*/
+  deck.innerHTML='';
+  /*Create the deck of cards*/
+  deckCreation();
+  /*add event listener to cards*/
+  addEvent();
+  /*close all cards*/
+  for (let i = 0; i < (cards.length); i++) {
+      cards[i].classList.remove("show", "open", "unmatch", "match");
+  }
+  /*reset moves*/
+    movesCounter = 0;
+    document.querySelector('.moves').innerHTML =movesCounter;
+  /*Reset the timer*/
+  	second = 0;
+  	minute = 0;
+    minutes.innerHTML = minute;
+    seconds.innerHTML = second;
+    /*reset rating*/
+    var emptyLength=emptyStars.length
+      for (let i = 0; i < emptyLength; i++){
+      emptyStars[0].setAttribute('class','fa fa-star');
+      }
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -56,31 +84,6 @@ function shuffle(array) {
     }
 
     return array;
-}
-
-//Function to start the game
-function startGame(){
-  shuffle(iconsList);
-  /*empty the deck if it already exists*/
-  deck.innerHTML='';
-  deckCreation();
-  addEvent();
-  /*close all cards*/
-  for (let i = 0; i < (cards.length); i++) {
-      cards[i].classList.remove("show", "open", "unmatch", "match");
-  }
-  /*reset moves*/
-    movesCounter = 0;
-    document.querySelector('.moves').innerHTML =movesCounter;
-  /*reset rating*/
-  	stars[0,1,2].setAttribute('class','fa fa-star');
-  /*Reset the timer*/
-  	clearInterval(interval);
-  	second = 0;
-  	minute = 0;
-  	hour = 0;
-    minutes.innerHTML = minute;
-    seconds.innerHTML = second;
 }
 
 // Create deck of cards
@@ -108,10 +111,9 @@ function addEvent () {
 }
 
 // Function for flipping card when clicked
-
 function flipCard() {
   //start timer
-  if (minute === 0 && hour === 0 && second === 0){
+  if (minute === 0 && second === 0){
 		startTimer();
 	}
   let cardListLength = flippedCardList.length;
@@ -120,17 +122,28 @@ function flipCard() {
     flippedCardList.push(this);
     //add open class to clicked card
     this.classList.add('open', 'show');
-    if(cardListLength != 0){
+    if(cardListLength !== 0){
     cardMatch();
     rating();
     }
   }
 }
 
-// Function for moves counter
-function addMove(){
-  movesCounter += 1;
-  document.querySelector('.moves').innerHTML = movesCounter;
+//Function for timer
+function startTimer(){
+  clearInterval(interval);
+	interval = setInterval(function(){
+    minutes.innerHTML = minute;
+    seconds.innerHTML = second;
+		second++;
+		if(second === 60){
+			second = 0;
+      minute++;
+	  }
+    if (minute > 10){
+      gameOver();
+    }
+	}, 1000);
 }
 
 // Function to test if cards are Matching
@@ -154,39 +167,24 @@ function cardMatch () {
   }
 }
 
+// Function for moves counter
+function addMove(){
+  movesCounter += 1;
+  document.querySelector('.moves').innerHTML = movesCounter;
+}
+
 // Function to change star rating
 function rating(){
-  if (movesCounter === 11){
+  if (movesCounter === 3){
     stars[2].setAttribute('class','fa fa-star-o');
   }
-  else if (movesCounter === 21){
+  else if (movesCounter === 6){
     stars[1].setAttribute('class','fa fa-star-o');
   }
-  else if (movesCounter === 31){
+  else if (movesCounter === 9){
     stars[0].setAttribute('class','fa fa-star-o');
   }
 }
-
-//Function for timer
-function startTimer(){
-	interval = setInterval(function(){
-    minutes.innerHTML = minute;
-    seconds.innerHTML = second;
-		second++;
-		if(second === 60){
-			second = 0;
-      minute++;
-	  }
-	  if(minute === 60){
-		  minute = 0;
-      hour++;
-		}
-    if (minute > 10){
-      gameOver();
-    }
-	}, 1000);
-}
-
 
 //Add event lister for click on restart
 restart.addEventListener('click', reset);
@@ -209,25 +207,25 @@ function winner(){
 		/*save the final star rating*/
 		let yourRating = document.querySelector(".stars").innerHTML;
     /*show modal popup*/
-		modal.style.display = "block";
+		winnerModal.style.display = "block";
 		/*show results on modal*/
 		document.getElementById("yourMoves").innerHTML = movesCounter;
 		document.getElementById("yourRating").innerHTML = yourRating;
 		document.getElementById("yourTime").innerHTML = yourTime;
-	};
-
-  //function to close the modal
-  let closeSpan = document.querySelector('.close');
-  closeSpan.onclick = function() {
-    modal.style.display = "none";
-    startGame();
-  }
+	}
 }
+
+/*function to close the winner the modal*/
+let closeSpan = document.querySelector('.close');
+closeSpan.onclick = function() {
+  winnerModal.style.display = "none";
+  reset();
+};
 
 //Start new game when clicking on button inside modal
 document.querySelector(".restartButton").addEventListener("click", function(){
-	modal.style.display = "none";
-	startGame();
+	winnerModal.style.display = "none";
+  reset();
 });
 
 
@@ -235,12 +233,14 @@ document.querySelector(".restartButton").addEventListener("click", function(){
 function gameOver(){
   clearInterval(interval);
   loserModal.style.display = "block";
-  //function to close the loser modal
+
+//function to close the loser modal
   let closeLoser = document.querySelector('.closeLoser');
   closeLoser.onclick = function() {
       loserModal.style.display = "none";
-      startGame();
-  }
+      reset();
+  };
+
   //Start new game when clicking on button inside loser modal
   document.querySelector(".restartLoser").addEventListener("click", function(){
     loserModal.style.display = "none";
@@ -250,14 +250,14 @@ function gameOver(){
 
 //close modal if the user clicks outside of the modal
 window.onclick = function (e) {
-  if (e.target == modal) {
-     modal.style.display = "none";
+  if (e.target == winnerModal) {
+     winnerModal.style.display = "none";
   }
   else if (e.target == loserModal) {
      loserModal.style.display = "none";
   }
-}
+};
 
 document.addEventListener('DOMContentLoaded', function() {
 startGame();
-})
+});
